be1a8a7f52096d1e9cf4500e0f99928a3f24d5e5
Sebastian906 <elsebas1912@gmail.com>
2025-07-25 13:40:44 -0500

    Función de obtener libros dentro del controlador

---
+++ Go-Bookstore/pkg/controllers/book-controller.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"encoding/json"
+	"go-bookstore/pkg/models"
+	"net/http"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
