6826bdde51d3e1cc7906f36df2b87225c81ab0c1
Sebastian906 <elsebas1912@gmail.com>
2025-07-25 16:16:51 -0500

    Se crea la función de obtener libros de acuerdo a su ID

--- Go-Bookstore/pkg/controllers/book-controller.go
+++ Go-Bookstore/pkg/controllers/book-controller.go
@@ -2,8 +2,12 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"go-bookstore/pkg/models"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
@@ -11,6 +15,20 @@ var NewBook models.Book
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
 	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId,0,0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
 	w.Header().Set("Content-Type","pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
