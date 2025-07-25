24768b50f81399b1e58645471926fc729ab007d9
Sebastian906 <elsebas1912@gmail.com>
2025-07-25 16:20:22 -0500

    Se crea la función de crear un libro

--- Go-Bookstore/pkg/controllers/book-controller.go
+++ Go-Bookstore/pkg/controllers/book-controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"go-bookstore/pkg/models"
+	"go-bookstore/pkg/utils"
 	"net/http"
 	"strconv"
 
@@ -30,6 +31,15 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	bookDetails, _ := models.GetBookById(ID)
 	res, _ := json.Marshal(bookDetails)
 	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
